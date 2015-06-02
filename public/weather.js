a05c92e170d93c1f61628d99e97c7834e400e586
Marcel Puyat <marcelp@alumni.stanford.edu>
2015-06-02 09:18:17 -0700

    Finish weeather page, using forecast.io api for icon text and downloading images to use

--- public/weather.js
+++ public/weather.js
@@ -4,6 +4,24 @@ $(document).ready(function() {
 	var minsPerForecastCall = 10;
 	var minsPerCurrTempCall = 5;
 
+	// See https://developer.forecast.io/docs/v2#forecast_call
+	var conditionsToIconMap = {
+		"clear-day": "/images/weather/clear-day.png",
+		"clear-night": "/images/weather/clear-night.png",
+		"rain": "/images/weather/rain.png",
+		"snow" : "/images/weather/snow.png",
+		"sleet": "/images/weather/snow.png",
+		"wind" : "/images/weather/wind.png",
+		"fog": "/images/weather/fog.png",
+		"cloudy": "/images/weather/fog.png",
+		"partly-cloudy-day": "/images/weather/partly-cloudy-day.png",
+		"partly-cloudy-night": "/images/weather/partly-cloudy-night.png",
+		"thunderstorm": "/images/weather/thunderstorm.png",
+		"hail": "/images/weather/snow.png",
+		"tornado": "/images/weather/wind.png",
+		"default": "images/weather/na.png"
+	};
+
 	// Ajax calls only modify the cache. Then we use the cache to populate DOM when the
 	// temperature page is added to the DOM
 	var cachedData = {};
@@ -31,7 +49,6 @@ $(document).ready(function() {
 	};
 
 	var updateForecastData = function(callback) {
-
 		$.ajax({
 	        url: 'http://api.wunderground.com/api/95e93303e496f7c9/forecast/q/'+state+'/'+encodeURIComponent(city)+'/.json',
 	        dataType: 'jsonp',
@@ -42,23 +59,38 @@ $(document).ready(function() {
 	            cachedData.low = forecastObject.low.fahrenheit;
 	            cachedData.icon = forecastObject.icon_url;
 	            cachedData.probOfRain = forecastObject.pop;
-	            if (callback) {
-	            	callback();
-	            }
+	            callback();
 	        }
 	    });
 	};
 
 	var updateCurrTemp = function(callback) {
+		var wundergroundDone = false;
+		var forecastIoDone = false;
 		$.ajax({
 	    	url: 'http://api.wunderground.com/api/95e93303e496f7c9/conditions/q/'+state+'/'+encodeURIComponent(city)+'.json',
 	    	dataType: 'jsonp',
 	        success: function(responseData) {
 	        	cachedData.currTemp = responseData.current_observation.feelslike_f;
-	        	if (callback) {
-	        		callback();
-	        	}
+	        	wundergroundDone = true;
+	            if (callback && wundergroundDone && forecastIoDone) {
+	            	callback();
+	            }
 	        }
+	    });
+
+		$.ajax({
+	    	// Coordinates here are hardcoded...
+	    	url: 'https://api.forecast.io/forecast/35c8a075c7e126ef14a4632a706ebbaf/37.8267,-122.423',
+	    	dataType: 'jsonp',
+	    	success: function(responseData) {
+	    		// See https://developer.forecast.io/docs/v2#forecast_call
+	    		cachedData.conditions = responseData.currently.icon;
+	    		forecastIoDone = true;
+	    		if (callback && wundergroundDone && forecastIoDone) {
+	            	callback();
+	            }
+	    	}
 	    });
 	};
 
@@ -71,7 +103,10 @@ $(document).ready(function() {
         $("#chance-of-rain-text").text(cachedData.probOfRain + "% ");
         var rainText = $('<span />').attr('class', 'small-weather-label').html('rain');
         $("#chance-of-rain-text").append(rainText);
-        $("#conditions-icon").attr("src", cachedData.icon);
+        var imgSrc = conditionsToIconMap[cachedData.conditions] ? conditionsToIconMap[cachedData.conditions] : 
+        	conditionsToIconMap.default;
+        $("#conditions-icon").attr("src", imgSrc);
+        console.log(conditionsToIconMap[cachedData.conditions]);
         $("#current-temp-text").text(Math.floor(cachedData.currTemp) + "\xB0");
         updateTemperatureColors();
 	}
